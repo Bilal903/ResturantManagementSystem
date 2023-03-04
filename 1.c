ffd2a90e2f9fb35080405b96ada75e1465fd60c8
Maddy <88387745+Bilal903@users.noreply.github.com>
2023-03-04 10:59:08 -0800

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,277 @@
+#include <stdio.h>
+int main_page();
+int menu_order();
+void Bill_show(int arID[],int arqty[],int counter);
+void Bill_Payment();
+
+int token = 0,tokens[50],payment_counter=0;
+
+int main()
+ {
+
+    int k,j,n,d;
+    k = main_page();
+
+    if(k==1)
+    {
+        menu_order();
+    }
+
+    else if(k==2)
+    {
+        Bill_Payment();
+    }
+
+    else
+    {
+      
+        printf("\n\n\n\n\n\t\t      \n\t\t           Cafe karachi\n\t\t\t\t\t    \n\n");
+        printf("\n\n\n\t\t\tThis session order recieved : %d\n\n",token);
+        printf("\t\t\tThis session bill paid      : %d\n\n",payment_counter);
+        /* if(payment_counter<token)
+        {
+            printf("\t\t\t\tYet to pay          : %d\n\n\n\n\n\n",token-payment_counter);
+        }
+        else
+        {
+            printf("\n\n\n\n\n\n");
+         }*/
+        return 0;
+    }
+
+ }
+
+
+int main_page()
+{
+    int i;
+    int n;
+    printf("\n\n\n\n\t\t    \n\t\t         Welcome to Cafe karachi \n\t\t\t\t\t  \n\n");
+    printf("\n\n\n\n\t\t\t    Good food.Great vibes.\n\n\t\t       ");
+
+    for (i=0;i<36;i++)
+     {
+        printf("~");
+     }
+    printf("\n");
+    printf("        tasty          $                                  $\n");
+    printf("                       $    Select any of them -          $\n");
+    printf("        and            $                                  $\n");
+    printf("                       $    1. New Order.                 $\n");
+    printf("       hygenic         $                                  $\n");
+    printf("                       $    2. Bill Payment.              $\n");
+    printf("      foods at         $                                  $\n");
+    printf("                       $    3. Exit.                      $");
+    printf("\n");
+    printf("      low cost..       $                                  $\n");
+    printf("                       $                                  $\n");
+    printf("     GRAB NOW !!       ");
+
+
+    for (i=0;i<36;i++)
+     {
+        printf("~");
+     }
+    printf("\n\n\n\n\t\t Enter Your Choice: ");
+    scanf("%d",&n);
+    return n;
+}
+
+
+
+int menu_order()
+{
+
+    int arID[10],arqty[10],Bill[10],counter=0,decision;
+
+    work:
+    printf("\n\t\t\t\tFOOD MENU\n\n");
+    printf("\n\t FAST FOOD :\n\n");
+    printf("\t1. zinger Burger ...................... 120/=\t  \n");
+    printf("\t2. VEG ROLL ..........................  40/=\n");
+    printf("\t3. Donuts ...........................  70/=\t \n");
+    printf("\t4. French Fries (FnF Pack) ..........  90/=\t \n");
+    printf("\t\t\t\t\t\t\t \n\n\t CHICKEN :\n\n");
+    printf("\t5. Chicken Broast ...................  90/=\n");
+    printf("\t6. Chicken Wings ....................  65/=\n");
+    printf("\n\tDESSERTS :\n\t\t\t\t\t\t\t \n");
+    printf("\t7. Ice Cream ........................  65/=\t  \n");
+    printf("\t8. pudding ........................... 230/=\t \n");
+    printf("\n\tDRINKS :\n\n");
+    printf("\t9.  7-up ........................ 25/=\n");
+    printf("\t10. pepsi   ........................ 25/=\n");
+
+    printf("\n\n\tOrder your dish: (Enter 0 to finish order)\n");
+
+    for(;;)
+    {
+        counter++;
+        printf("\t Enter Food ID: ");
+        scanf("%d",&arID[counter-1]);
+        if(arID[counter-1]==0)
+        {
+            break;
+        }
+        printf("\t Enter Quantity: ");
+        scanf("%d",&arqty[counter-1]);
+
+    }
+  
+    printf("\n\n\n\n\n\n\n\n\n\n\t\tThanks for your order. We are getting ready the dishes.\n\t\tUntil then,  Enjoy The ambiance!\n\n\n\n\n\t\t\t  <Enter 1 to check your bill>\n\t\t\t<Press 2 if you want to make changes in order>\n\t\t\t\n\t\tEnter : ");
+    scanf("%d",&decision);
+    printf("\n\n\n\n\n\n\n\n");
+    counter--;
+
+    if(decision==1)
+    {
+        Bill_show(arID,arqty,counter);
+    }
+
+     else if(decision==2)
+    {
+       goto work;
+    }
+
+    /* else if(decision==3)
+    {
+        main();
+    }*/
+
+}
+
+
+void Bill_show(int arID[],int arqty[],int counter)
+{
+
+    int n;
+
+    int i,sum=0;
+    int costs[10]={120,40,70,90,90,65,65,230,25,25};
+    char items[10][16]={"zinger Burger","VEG ROLL","Donuts","French Fries","Chicken Broast","Chicken Wings","Ice Cream","pudding","7-up","pepsi"};
+    printf("\n\nYour Bill:\n\n\n\t\tItem(s)\t\t     Quantity\t\t    Cost\n\n\n\n");
+
+    for(i=0;i<counter;i++)
+    {
+        printf("\t%2d. %-16s _\t%-5d piece(s) _ %7d\n",i+1,items[arID[i]-1],arqty[i],arqty[i]*costs[arID[i]-1]);
+        sum=sum+(costs[arID[i]-1]*arqty[i]);
+    }
+
+    printf("\n\n\n\n\t\t\t\t\t\t\t  Total = %d\n\n\n\n",sum);
+    token ++;
+    tokens[token]=sum;
+    printf("\tYour token number is %d. Use this token while paying the bill.\n\n\n\n\n\t\t\t<enter (1) to give another order>\n\n\t\t\t<enter (2) to go to the main menu>\n\n\t\n\t\t\tEnter your choice: ",token);
+    scanf("%d",&n);
+    printf("\n\n\n");
+    if(n==1)
+    {
+        menu_order();
+    }
+    else if(n==2)
+    {
+        main();
+
+    }
+
+}
+
+void Bill_Payment()
+{
+
+    int k,i,cash,changer,n;
+    redo:
+    payment_counter++;
+   
+    printf("\n\n\n\t\t\tEnter your token number: ");
+    scanf("%d",&k);
+    printf("\t\t\tYour Total Bill is = %d",tokens[k]);
+    work:
+            printf("\n\t\t\tPay your bill: ");
+            scanf("%d",&cash);
+    changer = cash-tokens[k];
+    if(changer == 0)
+    {
+        printf("\n\t\tThanks for your payment. Have a great day!\n");
+    }
+    else if (changer<0)
+    {
+        
+        printf("\n\t\t\tAmount is not sufficient. \n\t\tPlease pay the fair price of your bill\n");
+        goto work;
+    }
+    else
+    {
+        printf("\n\n\n\t\tHere is your change : (%d - %d) = %d RS.\n\t\t  ",cash,tokens[k],changer);
+
+            for(i=0;i<40;i++)
+            {
+                printf("_");
+            }
+            if(changer>=1000 )
+            {
+                printf("\n\t\t1000 RS. note(s) = %d\n",changer/1000);
+                changer=changer%1000;
+            }
+
+            if(changer>=500 && changer<1000)
+            {
+                printf("\n\t\t 500 RS. note(s) = %d\n",changer/500);
+                changer=changer%500;
+            }
+
+            if(changer>=100 && changer<500)
+            {
+                printf("\n\t\t 100 RS. note(s) = %d\n",changer/100);
+                changer= changer%100;
+            }
+
+            if(changer>=50 && changer<100)
+            {
+                printf("\n\t\t  50 RS. note(s) = %d\n",changer/50);
+                changer=changer%50;
+            }
+
+             if(changer>=10 && changer<50)
+            {
+                printf("\n\t\t  10 RS. note(s) = %d\n",changer/10);
+                changer=changer%10;
+            }
+
+            if(changer>=5 && changer<10)
+            {
+                printf("\n\t\t   5 RS. coins(s) = %d\n",changer/5);
+                changer=changer%5;
+            }
+
+            if(changer>=2 && changer<5)
+            {
+                printf("\n\t\t   2 RS. coins(s) = %d\n",changer/2);
+                changer=changer%2;
+            }
+
+            if(changer>=1 && changer<2)
+            {
+                printf("\n\t\t   1 RS. coins(s) = %d\n",changer/1);
+                changer=changer%1;
+            }
+
+            printf("\t\t  ");
+            for(i=0;i<40;i++)
+            {
+                printf("_");
+            }
+
+        printf("\n\n\n\n\n\t\t\tThanks for your payment.\n\t\t\t    Have a great day!\n",changer);
+
+    }
+        printf("\n\n\t\t\t<Enter (1) to pay another bill.>\n\t\t\t<Enter (2) to go back main menu>\n\n\n\t\t\tEnter your choice: \t");
+        scanf("%d",&n);
+        
+        if(n==2)
+        {
+             main();
+        }
+        else if(n==1)
+        {
+           goto redo;
+        }
+}
